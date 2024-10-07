f9fee2c277c4ca935973f22c98a11f7cf6f630d4
snahmd <sanahmed7@gmail.com>
2024-10-07 23:54:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Webmarkt Project
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-yellow-400 text-6xl text-center m-72">
+        Webmarkt Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
